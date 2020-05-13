962881eb69e8384dc8d01d7de5c188e85db14250
Peachex <klevolex@gmail.com>
2020-05-13 16:01:33 +0300

    fix task6

--- src/com/epam/task6/Main.java
+++ src/com/epam/task6/Main.java
@@ -3,25 +3,23 @@ package com.epam.task6;
 // 1. Дан текст (строка). Найдите наибольшее количество подряд идущих пробелов в нем.
 public class Main {
     public static void main(String[] args) {
-        String str = new String("1.   Дан текст (     строка)  . Найдите наибольшее      количество подряд идущих пробелов    в  нем.");
-        System.out.println("Наибольшее количество подряд идущих пробелов = " + maxSpaceNumber(str));
+        String str = "1.   Дан текст (     строка)  . Найдите наибольшее      количество подряд идущих пробелов    в  нем. ";
+
+        String[] spaceStr = str.split("[^\\s]");
+
+        System.out.println("Наибольшее количество подряд идущих пробелов = " + findMaxLength(spaceStr));
     }
 
-    public static int maxSpaceNumber(String str) {
-        int amount = 0;
-        int maxAmount = 0;
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == ' ') {
-                amount++;
-            } else {
-                if (amount > maxAmount) {
-                    maxAmount = amount;
-                }
-                amount = 0;
+    public static int findMaxLength(String[] str) {
+        int maxLength = str[0].length();
+
+        for (int i = 0; i < str.length; i++) {
+            if (str[i].length() > maxLength) {
+                maxLength = str[i].length();
             }
         }
 
-        return maxAmount;
+        return maxLength;
     }
 }
 
