79331e39d4d911929ac138fee723a7556787755a
Peachex <klevolex@gmail.com>
2020-05-13 16:24:57 +0300

    fix task7

--- src/com/epam/task7/Main.java
+++ src/com/epam/task7/Main.java
@@ -3,17 +3,9 @@ package com.epam.task7;
 // 2. В строке вставить после каждого символа 'a' символ 'b'.
 public class Main {
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder("aout, aove, aility, asurd, ale.");
+        String str1 = "aout, aove, aility, asurd, ale.";
+        String str2 = str1.replaceAll("a", "ab");
 
-        insert(sb);
-        System.out.println(sb);
-    }
-
-    public static void insert(StringBuilder sb) {
-        for (int i = 0; i < sb.length(); i++) {
-            if (sb.charAt(i) == 'a') {
-                sb.insert(i + 1, 'b');
-            }
-        }
+        System.out.println(str2);
     }
 }
