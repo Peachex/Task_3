d6fb6cac322b84e513fdc029beab8939b1b6c1e4
Peachex <klevolex@gmail.com>
2020-05-13 16:39:29 +0300

    fix task10

--- src/com/epam/task10/Main.java
+++ src/com/epam/task10/Main.java
@@ -3,19 +3,9 @@ package com.epam.task10;
 // 5. Подсчитать, сколько раз среди символов заданной строки встречается буква “а”.
 public class Main {
     public static void main(String[] args) {
-        String str = "5. Подсчитать, сколько раз среди символов заданной строки встречается буква “а”.";
-        System.out.println(toCount(str, 'а'));
-    }
+        String str1 = "5. Подсчитать, сколько раз среди символов заданной строки встречается буква “а”.";
 
-    public static int toCount(String str, char symbol) {
-        int symbolCount = 0;
-
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == symbol){
-                symbolCount++;
-            }
-        }
-
-        return symbolCount;
+        String str2 = str1.replaceAll("[^а]", "");
+        System.out.println(str2.length());
     }
 }
