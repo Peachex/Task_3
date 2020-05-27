4b0b11a8497592a1e0542a50aca0bda1d003fbf4
Peachex <klevolex@gmail.com>
2020-05-27 11:21:07 +0300

    fix task16

--- src/com/epam/task16/Main.java
+++ src/com/epam/task16/Main.java
@@ -1,7 +1,6 @@
 package com.epam.task16;
 
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 /* 1. Cоздать приложение, разбирающее текст (текст хранится в строке) и позволяющее выполнять с текстом три различных операции:
 отсортировать абзацы по количеству предложений; в каждом предложении отсортировать слова по длине; отсортировать лексемы в предложении
@@ -9,39 +8,62 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        StringBuilder sbText = new StringBuilder("Утро в середине ноября могло быть самым обыкновенным, если бы за ночь не выпало столько снега. Всё вокруг в один миг" +
-                " преобразилось, как в волшебной сказке. Белоснежные сугробы покрыли мостовые, аллеи в парках, узкие дорожки во дворах, машины," +
-                " как будто кто-то накинул большую накрахмаленную простыню на город. Невесомые снежинки, вздрагивая от малейшего порыва ветра, " +
-                "искрятся и сверкают миллионами огней в лучах мягкого зимнего солнца, словно драгоценные кристаллы.\n\nПервый снег - это всегда" +
-                " радость, настоящее преображение природы. Зима вступила в свои права, и это её щедрый подарок. Хлопья снега сыпятся из тяжелых " +
-                "нависших над городом туч и пушистым ковром ложатся на улицы. Воздух свеж и чист. Голые ветки деревьев ранним утром покрывает " +
-                "колючий иней, придавая им причудливую форму. Тонкие заледеневшие иголочки красиво сверкают и переливаются разноцветными огнями. " +
-                "Не деревья в парке, а заколдованный лес! Морозное дыхание зимы рисует на стёклах затейливый орнамент, и окна сразу приобретают " +
-                "сказочный вид.\n\nТихо. Всё вокруг замирает. Деревья, тянущие изогнутые ветви к небу, напоминают заснеженных молчаливых " +
-                "истуканов. Но стоит приблизиться, и волшебство исчезает. Такая хрупкая красота! Ажурные снежинки осыпятся от лёгкого " +
-                "прикосновения, оголив тёмные ветки. Но за ночь чародейка-зима всё исправит, чтобы утром все снова смогли насладиться" +
-                " первозданной красотой.");
 
-        viewText(sbText);
-        System.out.println();
+        String text = "Утро в середине ноября могло быть самым обыкновенным, если бы за ночь не выпало столько снега. Всё вокруг в один миг преобразилось, как в волшебной сказке. Белоснежные сугробы покрыли мостовые, аллеи в парках, узкие дорожки во дворах, машины, как будто кто-то накинул большую накрахмаленную простыню на город. Невесомые снежинки, вздрагивая от малейшего порыва ветра, искрятся и сверкают миллионами огней в лучах мягкого зимнего солнца, словно драгоценные кристаллы. \n" +
+                "Первый снег — это всегда радость, настоящее преображение природы. Зима вступила в свои права, и это её щедрый подарок. Хлопья снега сыпятся из тяжелых нависших над городом туч и пушистым ковром ложатся на улицы. Воздух свеж и чист. Голые ветки деревьев ранним утром покрывает колючий иней, придавая им причудливую форму. Тонкие заледеневшие иголочки красиво сверкают и переливаются разноцветными огнями. Не деревья в парке, а заколдованный лес! Морозное дыхание зимы рисует на стёклах затейливый орнамент, и окна сразу приобретают сказочный вид.\n" +
+                "Тихо. Всё вокруг замирает. Деревья, тянущие изогнутые ветви к небу, напоминают заснеженных молчаливых истуканов. Но стоит приблизиться, и волшебство исчезает. Такая хрупкая красота! Ажурные снежинки осыпятся от лёгкого прикосновения, оголив тёмные ветки. Но за ночь чародейка-зима всё исправит, чтобы утром все снова смогли насладиться первозданной красотой.\n" +
+                "\n";
 
-        int point;
-        boolean flag = true;
+        System.out.println(text);
 
-        while (flag) {
+        String[][][] textArray = new String[text.split("\n").length][][];
+
+        String[] paragraphsArray = text.split("\n");
+
+        for (int i = 0; i < paragraphsArray.length; i++) {
+            String[] sentencesArray = paragraphsArray[i].split("\\.\\s|\\?\\s|!\\s");
+            textArray[i] = new String[sentencesArray.length][];
+
+            for (int j = 0; j < paragraphsArray[i].split("\\.\\s|\\?\\s|!\\s").length; j++) {
+                String[] wordsArray = sentencesArray[j].split("[\\s,—]");
+                textArray[i][j] = new String[wordsArray.length];
+
+                for (int k = 0; k < sentencesArray[j].split("[\\s,—]").length; k++) {
+                    textArray[i][j][k] = wordsArray[k];
+                }
+            }
+        }
+
+        int point1;
+        int point2;
+
+        boolean flag1 = true;
+        boolean flag2;
+
+        while (flag1) {
             System.out.println("1)Отсортировать абзацы по количеству предложений.\n2)Отсортировать в каждом предложении слова по длине.\n3)Отсортировать лексемы в предложении " +
                     "по убыванию количества вхождений заданного символа, а в случае равенства – по алфавиту.\n4)Выход.");
 
-            point = in.nextInt();
+            point1 = in.nextInt();
 
-            switch (point) {
+            switch (point1) {
                 case 1: {
-                    viewText(sortParagraphs(sbText));
+                    System.out.println("1)По возрастанию.\n2)По убыванию.");
+                    point2 = in.nextInt();
+
+                    flag2 = point2 == 1 ? true : false;
+
+                    sortParagraphs(textArray, flag2);
                     break;
                 }
 
                 case 2: {
-                    viewText(sortWords(sbText));
+                    System.out.println("1)По возрастанию.\n2)По убыванию.");
+                    point2 = in.nextInt();
+
+                    flag2 = point2 == 1 ? true : false;
+
+                    sortWordsByLength(textArray, flag2);
                     break;
                 }
 
@@ -49,87 +71,101 @@ public class Main {
                     System.out.println("Введите символ: ");
                     String str = in.next();
 
-                    viewText(sortLetters(sbText, str.charAt(0)));
+                    sortWordsByNumberOfEntries(textArray, str.charAt(0));
                     break;
                 }
 
                 case 4: {
-                    flag = false;
-                }
-            }
-        }
-
-    }
-    
-    public static void viewText(StringBuilder sb) {
-        System.out.println(sb);
-    }
-
-    public static String[] split(String regex, StringBuilder sbText) {
-        Pattern pattern = Pattern.compile(regex);
-
-        return pattern.split(sbText);
-    }
-
-    public static void sortWordsByLength(String[] array) {
-        String tempStr;
-        for (int count = 0; count < array.length; count++) {
-            for (int i = 0; i < array.length - 1; i++) {
-                if (array[i].length() > array[i + 1].length()) {
-                    tempStr = array[i];
-                    array[i] = array[i + 1];
-                    array[i + 1] = tempStr;
+                    flag1 = false;
                 }
             }
         }
     }
 
-    public static String addToString(String[] array, char symbol) {
-        StringBuilder sb = new StringBuilder();
+    public static void sortParagraphs(String[][][] textArray, boolean flag) {
+        int[] sentencesAmountArray = new int[textArray.length];
+        int[] indexArray = new int[textArray.length];
 
-        for (int i = 0; i < array.length; i++) {
-            if (i < array.length - 1) {
-                sb.append(array[i] + " ");
-            } else {
-                sb.append(array[i]);
-            }
+        for (int i = 0; i < sentencesAmountArray.length; i++) {
+            sentencesAmountArray[i] = textArray[i].length;
+            indexArray[i] = i;
         }
 
-        sb.append(symbol);
+        sortArray(sentencesAmountArray, indexArray, flag);
 
-        return sb.toString();
-    }
-
-    public static void sortWordsByNumberOfEntries(String[] array, char symbol) {
-        int[] numberOfEntriesArray = new int[array.length];
-
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length(); j++) {
-                if (array[i].charAt(j) == symbol) {
-                    numberOfEntriesArray[i]++;
+        for (int i = 0; i < textArray.length; i++) {
+            for (int j = 0; j < textArray[indexArray[i]].length; j++) {
+                for (int k = 0; k < textArray[indexArray[i]][j].length; k++) {
+                    System.out.print(" " + textArray[indexArray[i]][j][k]);
                 }
             }
+            System.out.println();
+        }
+    }
+
+    public static void sortWordsByLength(String[][][] textArray, boolean flag) {
+        for (int i = 0; i < textArray.length; i++) {
+            for (int j = 0; j < textArray[i].length; j++) {
+                int[] wordsLengthArray = new int[textArray[i][j].length];
+                int[] indexArray = new int[textArray[i][j].length];
+
+                for (int k = 0; k < textArray[i][j].length; k++) {
+                    wordsLengthArray[k] = textArray[i][j][k].length();
+                    indexArray[k] = k;
+                }
+
+                sortArray(wordsLengthArray, indexArray, flag);
+
+                for (int k = 0; k < textArray[i][j].length; k++) {
+                    System.out.print(" " + textArray[i][j][indexArray[k]]);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void sortWordsByNumberOfEntries(String[][][] textArray, char character) {
+        for (int i = 0; i < textArray.length; i++) {
+            for (int j = 0; j < textArray[i].length; j++) {
+                int[] numberOfEntriesArray = new int[textArray[i][j].length];
+                int[] indexArray = new int[textArray[i][j].length];
+
+                for (int k = 0; k < textArray[i][j].length; k++) {
+                    numberOfEntriesArray[k] = numberOfEntries(textArray, i, j, k, character);
+                    indexArray[k] = k;
+                }
+
+                sortNumberOfEntriesArray(numberOfEntriesArray, indexArray, textArray, i, j);
+
+                for (int k = 0; k < textArray[i][j].length; k++) {
+                    System.out.print(" " + textArray[i][j][indexArray[k]]);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static int numberOfEntries(String[][][] textArray, int i, int j, int k, char character) {
+        int entriesAmount = 0;
+
+        for (int count = 0; count < textArray[i][j][k].length(); count++) {
+            if (textArray[i][j][k].charAt(count) == character) {
+                entriesAmount++;
+            }
         }
 
-        String tempStr;
-        int tempInt;
+        return entriesAmount;
+    }
 
-        for (int count = 0; count < array.length; count++) {
-            for (int i = 0; i < array.length - 1; i++) {
-                if (numberOfEntriesArray[i] < numberOfEntriesArray[i + 1]) {
-                    tempStr = array[i];
-                    array[i] = array[i + 1];
-                    array[i + 1] = tempStr;
-
-                    tempInt = numberOfEntriesArray[i];
-                    numberOfEntriesArray[i] = numberOfEntriesArray[i + 1];
-                    numberOfEntriesArray[i + 1] = tempInt;
+    public static void sortNumberOfEntriesArray(int[] numberOfEntriesArray, int[] indexArray, String[][][] textArray, int i, int j) {
+        for (int a = 0; a < numberOfEntriesArray.length; a++) {
+            for (int b = 0; b < numberOfEntriesArray.length - 1; b++) {
+                if (numberOfEntriesArray[b] < numberOfEntriesArray[b + 1]) {
+                    replace(numberOfEntriesArray, indexArray, b);
                 } else {
-                    if (numberOfEntriesArray[i] == numberOfEntriesArray[i + 1] && numberOfEntriesArray[i] != 0) {
-                        if (array[i].compareToIgnoreCase(array[i + 1]) > 0) {
-                            tempStr = array[i];
-                            array[i] = array[i + 1];
-                            array[i + 1] = tempStr;
+                    if (numberOfEntriesArray[b] == numberOfEntriesArray[b + 1] && numberOfEntriesArray[b] != 0) {
+                        if (textArray[i][j][indexArray[b]].compareToIgnoreCase(textArray[i][j][indexArray[b + 1]]) > 0) {
+                            replace(numberOfEntriesArray, indexArray, b);
                         }
                     }
                 }
@@ -137,112 +173,29 @@ public class Main {
         }
     }
 
-    public static StringBuilder sortParagraphs(StringBuilder sbText) {
-        String[] paragraphsArray = split("\\s\\s", sbText);
-
-        int[] sentencesAmount = new int[paragraphsArray.length];
-        int sentencesCount = 0;
-
-        for (int i = 0; i < paragraphsArray.length; i++) {
-            for (int j = 0; j < paragraphsArray[i].length(); j++) {
-                if (paragraphsArray[i].charAt(j) == '.' || paragraphsArray[i].charAt(j) == '?' || paragraphsArray[i].charAt(j) == '!') {
-                    sentencesCount++;
-                }
-            }
-            sentencesAmount[i] = sentencesCount;
-            sentencesCount = 0;
-        }
-
-        String tempStr;
-        int tempInt;
-
-        for (int count = 0; count < paragraphsArray.length; count++) {
-            for (int j = 0; j < paragraphsArray.length - 1; j++) {
-                if (sentencesAmount[j] > sentencesAmount[j + 1]) {
-                    tempStr = paragraphsArray[j];
-                    paragraphsArray[j] = paragraphsArray[j + 1];
-                    paragraphsArray[j + 1] = tempStr;
-
-                    tempInt = sentencesAmount[j];
-                    sentencesAmount[j] = sentencesAmount[j + 1];
-                    sentencesAmount[j + 1] = tempInt;
+    public static void sortArray(int[] array1, int[] array2, boolean flag) {
+        for (int i = 0; i < array1.length; i++) {
+            for (int j = 0; j < array1.length - 1; j++) {
+                if (flag) {
+                    if (array1[j] > array1[j + 1]) {
+                        replace(array1, array2, j);
+                    }
+                } else {
+                    if (array1[j] < array1[j + 1]) {
+                        replace(array1, array2, j);
+                    }
                 }
             }
         }
-
-        sbText = new StringBuilder();
-
-        System.out.println("Количество предложений в абзаце:");
-
-        for (int i = 0; i < paragraphsArray.length; i++) {
-            System.out.println((i + 1) + ")" + sentencesAmount[i]);
-            sbText.append(paragraphsArray[i] + "\n\n");
-        }
-
-        return sbText;
     }
 
-    public static StringBuilder sortWords(StringBuilder sbText) {
-        String[] paragraphsArray = split("\\s\\s", sbText);
+    public static void replace(int[] array1, int[] array2, int i) {
+        int temp = array1[i];
+        array1[i] = array1[i + 1];
+        array1[i + 1] = temp;
 
-        StringBuilder sb;
-
-        for (int i = 0; i < paragraphsArray.length; i++) {
-            sb = new StringBuilder(paragraphsArray[i]);
-            String[] sentencesArray = split("\\.|!|\\?", sb);
-
-            int countPunct = 0;
-
-            for (int j = 0; j < sentencesArray.length; j++) {
-                countPunct += sentencesArray[j].length();
-                sb = new StringBuilder(sentencesArray[j]);
-                String[] wordsArray = split("\\p{Blank}|\\,|\\- ", sb);
-                sortWordsByLength(wordsArray);
-                sentencesArray[j] = addToString(wordsArray, paragraphsArray[i].charAt(countPunct));
-                countPunct++;
-            }
-
-            paragraphsArray[i] = addToString(sentencesArray, ' ');
-        }
-
-        sbText = new StringBuilder();
-        for (int i = 0; i < paragraphsArray.length; i++) {
-            sbText.append(paragraphsArray[i] + "\n\n");
-        }
-
-        return sbText;
-    }
-
-    public static StringBuilder sortLetters(StringBuilder sbText, char symbol) {
-        String[] paragraphsArray = split("\\s\\s", sbText);
-
-        StringBuilder sb;
-
-        int countPunct;
-
-        for (int i = 0; i < paragraphsArray.length; i++) {
-            sb = new StringBuilder(paragraphsArray[i]);
-            String[] sentencesArray = split("\\.|!|\\?", sb);
-            countPunct = 0;
-
-            for (int j = 0; j < sentencesArray.length; j++) {
-                countPunct += sentencesArray[j].length();
-                sb = new StringBuilder(sentencesArray[j]);
-                String[] wordsArray = split("\\p{Blank}|\\,|\\- ", sb);
-                sortWordsByNumberOfEntries(wordsArray, symbol);
-                sentencesArray[j] = addToString(wordsArray, paragraphsArray[i].charAt(countPunct));
-                countPunct++;
-            }
-
-            paragraphsArray[i] = addToString(sentencesArray, ' ');
-        }
-
-        sbText = new StringBuilder();
-
-        for (int i = 0; i < paragraphsArray.length; i++) {
-            sbText.append(paragraphsArray[i] + "\n\n");
-        }
-
-        return sbText;
+        temp = array2[i];
+        array2[i] = array2[i + 1];
+        array2[i + 1] = temp;
     }
 }
