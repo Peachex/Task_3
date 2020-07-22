d8a3b7404bdf5cb554d6ae74db0e1f7e6bb38e32
Peachex <klevolex@gmail.com>
2020-07-22 09:21:50 +0300

    fix variable type

--- src/com/epam/task7/Main.java
+++ src/com/epam/task7/Main.java
@@ -3,9 +3,7 @@ package com.epam.task7;
 // 2. В строке вставить после каждого символа 'a' символ 'b'.
 public class Main {
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("aout, aove, aility, asurd, ale.");
-
-        System.out.println(sb.toString().replaceAll("a", "ab"));
+        String str = "aout, aove, aility, asurd, ale.";
+        System.out.println(str.replaceAll("a", "ab"));
     }
 }
